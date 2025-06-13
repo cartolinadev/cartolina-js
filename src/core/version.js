9c1a12c5b939291a6dd4cad7b03c9313a465e189
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-06-13 20:43:06 +0200

    fixed default export

--- src/core/version.js
+++ src/core/version.js
@@ -1,6 +1,6 @@
 // This file is auto-generated. Do not edit manually.
 // Use scripts/bump-patch.js to update version.
 
-export function getVersion() {
-  return '2.40.6-ondra_cartolina23.4e69994';
+export default function getVersion() {
+  return '2.40.7-ondra_cartolina23.cebca3e';
 }
