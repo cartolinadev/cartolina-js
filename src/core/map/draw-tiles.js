cartolinadev/cartolina-js

commit 7609371df55142abcc8dd18ac1686a35483cfd81

more comments and non-semantic changes, fixed config.mapGridTextureLayer

src/core/map/draw-tiles.js
@@ -717,6 +717,7 @@ MapDrawTiles.prototype.updateTileHmap = function(tile, node) {
 
 
 MapDrawTiles.prototype.updateTileBounds = function(tile, submeshes) {
+
     for (var i = 0, li = submeshes.length; i < li; i++) {
         var submesh = submeshes[i];
         
@@ -755,13 +756,13 @@ MapDrawTiles.prototype.updateTileBounds = function(tile, submeshes) {
                 } 
                 
                 if (bounds.viewCoutner != tile.viewCoutner) {
-                    this.updateTileSurfaceBounds(tile, submesh, submeshSurface, bounds, bounds.viewCoutner != tile.viewCoutner);
+                    this.updateTileSurfaceBounds(tile, submesh, submeshSurface, bounds);
                     //bounds.viewCoutner = tile.viewCoutner;
                     //console.log(bounds);
                 }  
             }
         } // if (submesh.externalUVs)
-    }
+    } // for (var i = 0, li = submeshes.length; i < li; i++)
 
     for (var key in tile.bounds) {
         tile.bounds[key].viewCoutner = tile.viewCoutner;
@@ -787,7 +788,7 @@ MapDrawTiles.prototype.getTileTextureTransform = function(sourceTile, targetTile
 };
 
 
-MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface, bound, fullUpdate) {
+MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface, bound) {
     var path, extraBound, layer, texture;
 
     if (this.config.mapNoTextures) {
@@ -797,7 +798,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
     // bump maps
     bound.bumps = [];
 
-    if (fullUpdate)
+    //if (fullUpdate)
         for (let j = 0; j < surface.bumpSequence.length; j++) {
 
         let bump = surface.bumpSequence[j];
@@ -835,7 +836,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
     }
 
     // specular maps
-    if (surface.specularSequence.length > 0 && fullUpdate) {
+    if (surface.specularSequence.length > 0) {
 
         bound.speculars = [];
         let sequenceFullAndOpaque = [];
@@ -843,6 +844,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
         let fullAndOpaqueCounter = 0;
 
         for (let j = 0, lj = surface.specularSequence.length; j < lj; j++) {
+
 
             let specular = surface.specularSequence[j];
 
@@ -863,6 +865,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
                 texture = tile.boundTextures[specular.layer.id];
 
                 if (!texture) {
+
                     path = specular.layer.getUrl(tile.id);
 
                     texture = tile.resources.getTexture(path,
@@ -912,7 +915,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
     //search map view
     if (surface.boundLayerSequence.length > 0) {
 
-        if (fullUpdate) {
+        //if (fullUpdate) {
             bound.sequence = [];
             var sequenceFullAndOpaque = [];
             var sequenceMaskPosible = [];
@@ -1006,7 +1009,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
                         break; //wait until mask info is loaded
                     }
                 }
-            }
+            } // for (var j = 0; lj = surface.boundLayerSequence.length; j < lj; j++)
 
             //filter out extra bounds if they are not needed
             //and remove all layer after first FullAndOpaque 
@@ -1035,12 +1038,15 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
                 bound.sequence = newSequence; 
             }
 
-        } // if (fullUpdate)
+        //} // if (fullUpdate)
 
 
     } // if (surfaceBoundLayerSequence.length > 0)
 
     else if (surface.textureLayer != null) { //search surface
+
+        // if (surfaceBoundLayerSequence.length == 0)
+
         if (fullUpdate) {
             layer = this.map.getBoundLayerById(surface.textureLayer);
             if (layer && layer.hasTileOrInfluence(tile.id)) {

src/core/map/texture.js
@@ -10,16 +10,16 @@ var MapTexture = function(map, path, type, extraBound, extraInfo, tile, internal
     this.stats = map.stats;
     this.tile = tile; // used only for stats
     this.internal = internal; // used only for stats
-    
+
     if (tile) {
-        this.mainTexture = tile.resources.getSubtexture(this, path, type, tile, internal); 
+        this.mainTexture = tile.resources.getSubtexture(this, path, type, null, null, tile, internal);
     } else {
         this.mainTexture = new MapSubtexture(map, path, type, tile, internal); 
     }
 
     this.maskTexture = null; 
 
-    this.loadState = 0;
+    this.loadState = 0; //  0=not loaded, 1=loading, 2=loaded, 3=error),
     this.loadErrorTime = null;
     this.loadErrorCounter = 0;
     this.neverReady = false;
@@ -29,7 +29,7 @@ var MapTexture = function(map, path, type, extraBound, extraInfo, tile, internal
     this.extraBound = extraBound;
     this.extraInfo = extraInfo;
     this.statsCounter = 0;
-    this.checkStatus = 0;
+    this.checkStatus = 0; // pre-load checks: 0=not checked, 1=in progress, 2=pass,-1=fail depending on the check)
     this.checkType = null;
     this.checkValue = null;
     this.fastHeaderCheck = false;
@@ -132,6 +132,7 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
    
     if (this.extraBound) {
         if (this.extraBound.texture) {
+
             while (this.extraBound.texture.extraBound || this.extraBound.texture.checkStatus == -1) {
 //            while (this.extraBound.texture.checkStatus == -1) {
                 parent = this.extraBound.sourceTile.parent;
@@ -164,13 +165,18 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
 
             return ready;
             
-        } else {
+        } // if (this.extraBound.texture)
+
+        else {
+
+            // if (! this.extraBound.texture)
+
             this.setBoundTexture(this.extraBound.sourceTile, this.extraBound.layer, this.extraBound.hmap);        
             return this.isReady(doNotLoad, priority, doNotCheckGpu);
         }
         
         return false;
-    }
+    } // if (this.extraBound)
 
     /*
     if (!this.extraBound && this.extraInfo && !this.maskTexture) {
@@ -185,8 +191,8 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
     switch (this.checkType) {
     case VTS_TEXTURECHECK_MEATATILE:
 
-        if (this.checkStatus != 2) {
-            if (this.checkStatus == 0) {
+//        if (this.checkStatus != 2) {
+            if (this.checkStatus == 0) { // not checked
                 if (this.extraInfo && this.extraInfo.tile) {
                     var metaresources = this.extraInfo.tile.boundmetaresources;
                     if (!metaresources) {
@@ -196,13 +202,15 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
                         
                     var layer = this.extraInfo.layer;
                     var path = this.extraInfo.metaPath;
-						
+
                     if(!this.extraInfo.metaPath) {
                         path = layer.getMetatileUrl(metaresources.id);	
                         this.extraInfo.metaPath = path;
                     }
-						
-                    var texture = metaresources.getTexture(path, true, null, null, this.tile, this.internal);
+
+                    // why are bound layer metatiles loaded as heightfields (true == VTS_TEXTURETYPE_HEIGHT)?
+                    var texture = metaresources.getTexture(path,
+                        VTS_TEXTURETYPE_HEIGHT, null, null, this.tile, this.internal);
                         
                     if (this.maskTexture) {
                         if (this.maskTexture.isReady(doNotLoad, priority, doNotCheckGpu, this)) {
@@ -229,7 +237,7 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
                 }
             }
                 
-            if (this.checkStatus == -1) {
+            if (this.checkStatus == -1) { // fail
                 if (!this.extraBound) {
                     parent = this.extraInfo.tile.parent;
                     if (parent.id[0] < this.extraInfo.layer.lodRange[0]) {
@@ -258,11 +266,13 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
                 }
             }
 
-            return false;
-        }
+            if (this.checkStatus == 1) { // in progress
+                return false;
+            }
+//        } // if (this.checkStatus != 2)
         
         break;
-    }
+    } // switch (this.checkType)
 
     var maskState = true;
 

src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.40.19-ondra_cartolina23.6f28a63';
+  return '2.40.20-ondra_cartolina23.a25f14e';
 }

src/core/map/subtexture.js
@@ -12,8 +12,8 @@ var MapSubtexture = function(map, path, type, tile, internal) {
     this.stats = map.stats;
     this.tile = tile; // used only for stats
     this.internal = internal; // used only for stats
-    this.image = null;
-    this.imageData = null;
+    this.image = null; // image bitmap (browser-native image container)
+    this.imageData = null; // raw RGBA byte array (height, metatile)
     this.imageExtents = null;
     this.gpuTexture = null;
     this.loadState = 0;
@@ -165,7 +165,9 @@ MapSubtexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu, t
     if (this.loadState == 2) { //loaded
         var t;
 
+        // keep the item warm in resource cache
         if (!doNotLoad && this.cacheItem) {
+
             this.map.resourcesCache.updateItem(this.cacheItem);
         }
 
@@ -190,6 +192,9 @@ MapSubtexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu, t
         }
 
         if (this.type == VTS_TEXTURETYPE_HEIGHT) {
+
+            //console.log(this.type, this.mapLoaderUrl);
+
             if (!this.imageData) {
                 t = performance.now();
                 this.buildHeightMap();
@@ -297,6 +302,7 @@ MapSubtexture.prototype.onLoadError = function(killBlob) {
 
 
 MapSubtexture.prototype.onBinaryLoaded = function(data, direct, filesize) {
+
     if (this.fastHeaderCheck && this.checkType && this.checkType != VTS_TEXTURECHECK_MEATATILE) {
         this.onHeadLoaded(null, data, null /*status*/);
         
@@ -306,27 +312,39 @@ MapSubtexture.prototype.onBinaryLoaded = function(data, direct, filesize) {
         }
     }
 
+
     if (direct) {
+        // worker decoded image (mapAsynbcImageDecode), blob is bitmap
         this.onLoaded(false, data)
         this.fileSize = filesize;
         return;
     }
 
+    // main thread decoding, data is blob
     this.fileSize = data.size;
 
-    if (this.map.config.mapAsyncImageDecode) {
+    createImageBitmap(data)
+        .then(this.onLoaded.bind(this,false))
+        .catch(this.onLoadError.bind(this, false));
+
+/*    if (this.map.config.mapAsyncImageDecode) {
+        // data (from worker) is an image bitmap
         createImageBitmap(data).then(this.onLoaded.bind(this, false));
+
     } else {
+        // data (from worker) is a xhr blob
+
         var image = new Image();
         image.onerror = this.onLoadError.bind(this, true, null);
         image.onload = this.onLoaded.bind(this, true, null);
         this.image = image;
         image.src = window.URL.createObjectURL(data);
-    }
+    }*/
 };
 
 
 MapSubtexture.prototype.onLoaded = function(killBlob, bitmap) {
+
     if (this.map.killed){
         return;
     }

src/core/map/surface-tile.js
@@ -1368,11 +1368,15 @@ MapSurfaceTile.prototype.drawGrid = function(cameraPos, divNode, angle, onlySetB
             if (!layer || sourceTile < layer.lodRange[0]) {
                 useTexture = false;
             } else {
-                var sourceLod = math.clamp(sourceTile.id[0] - map.config.mapGridTextureLevel, layer.lodRange[0], layer.lodRange[3]);
+                var sourceLod = math.clamp(sourceTile.id[0] - map.config.mapGridTextureLevel, layer.lodRange[0], layer.lodRange[1]);
+
+                console.log(layer.lodRange);
 
                 while (sourceTile.id[0] > sourceLod) {
                     sourceTile = sourceTile.parent;
                 }
+
+                console.log("gridTexture id: %s -> %s", this.id, sourceTile.id);
 
                 //(path, type, extraBound, extraInfo, tile, internal)
                 this.gridTexture = this.resources.getTexture("gmap#"+map.config.mapGridTextureLayer, null, {sourceTile: sourceTile, layer:layer, tile: this }, null, null, null);

src/core/map/surface-tree.js
@@ -776,7 +776,7 @@ MapSurfaceTree.prototype.drawSurfaceFitOnly = function(shift, storeTilesOnly) {
 
                 if (/*node.hasGeometry() && */tile.texelSize <= texelSizeFit) {
 
-                    console.log("Drawing %s (fit)", tile.id);
+                    //console.log("Drawing %s (fit)", tile.id);
 
                     tile.drawCounter = draw.drawCounter;
                     drawBuffer[drawBufferIndex] = tile;
@@ -833,8 +833,8 @@ MapSurfaceTree.prototype.drawSurfaceFitOnly = function(shift, storeTilesOnly) {
                     } else {
 
                         // if (childrenCount == 0
-                        console.log("Drawing %s (fallback), childrenCount = %d, nodesReadyCount = %d",
-                                    tile.id, childrenCount, nodesReadyCount);
+                        //console.log("Drawing %s (fallback), childrenCount = %d, nodesReadyCount = %d",
+                        //            tile.id, childrenCount, nodesReadyCount);
 
                         tile.drawCounter = draw.drawCounter;
                         drawBuffer[drawBufferIndex] = tile;

commit e8d465b08d38839bdb6e6c69f2c12c7bc2a785a4

fixed the bug introduced in 7609371df55142abcc8dd18ac1686a35483cfd81

src/core/map/draw-tiles.js
@@ -1039,7 +1039,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
 
         // if (surfaceBoundLayerSequence.length == 0)
 
-        if (fullUpdate) {
+        //if (fullUpdate) {
             layer = this.map.getBoundLayerById(surface.textureLayer);
             if (layer && layer.hasTileOrInfluence(tile.id)) {
                 extraBound = null; 
@@ -1060,7 +1060,7 @@ MapDrawTiles.prototype.updateTileSurfaceBounds = function(tile, submesh, surface
                     tile.boundTextures[layer.id] = tile.resources.getTexture(path, layer.dataType, extraBound, {tile: tile, layer: layer}, tile, false);
                 }
             }
-        }
+        //}
 
 
 

src/core/map/texture.js
@@ -227,6 +227,8 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
                         }
                     }
                 } // if (this.extraInfo && this.extraInfo.tile)
+
+                return false;
             }
                 
             if (this.checkStatus == -1) { // fail
@@ -256,9 +258,16 @@ MapTexture.prototype.isReady = function(doNotLoad, priority, doNotCheckGpu) {
                         
                     this.setBoundTexture(parent, this.extraBound.layer);        
                 }
+
+                return false;
             }
 
             if (this.checkStatus == 1) { // in progress
+                return false;
+            }
+
+            if (this.checkStatus != 2) { // sanity
+                console.log('WARN: unknown check status.');
                 return false;
             }
 //        } // if (this.checkStatus != 2)

src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.40.23-ondra_cartolina23.06b9b0d';
+  return '2.40.24-ondra_cartolina23.69c5e60';
 }