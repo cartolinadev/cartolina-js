fa4b12e0a75f5a6d55804e6a33d37e1ed34f54e3
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-08-25 23:01:16 +0200

    default search template uses nominatim, default font is noto-extended

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.40.25-ondra_cartolina23.6662684';
+  return '2.40.26-ondra_cartolina23.a0dd2a8';
 }
