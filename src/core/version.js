2a2cbb3d6c6dfc2f27cc836eb2db9bd6594d1f6c
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-08-31 01:50:19 +0200

    automatic regression tests now use the same test/urls.json file as visual regression

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.40.34-ondra_cartolina23.62df242';
+  return '2.40.35-ondra_cartolina23.c9d761a';
 }
