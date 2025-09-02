0f055aea6e300a4af5283240d1dc52f7eb54767b
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-09-02 19:00:58 +0200

    minor stuff in gputexture

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.49.4-ondra_cartolina23.23f8f67';
+  return '2.49.5-ondra_cartolina23.2397200';
 }
