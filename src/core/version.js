a6819d62b5032f90d790b385c8564de94082d7fe
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-09-11 21:44:51 +0200

    fixed bug introduced when moving credit handling to TileRenderRig

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.49.31-ondra_cartolina23.b2aafe8';
+  return '2.49.32-ondra_cartolina23.8e2ac3a';
 }
