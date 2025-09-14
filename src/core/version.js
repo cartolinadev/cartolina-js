55a33cb78f2862cd470ee5a59e9f40f9c858a8e0
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-09-14 22:28:53 +0200

    tile render rig wip

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.49.42-ondra_cartolina23.294a8b2';
+  return '2.49.43-ondra_cartolina23.58fce86';
 }
