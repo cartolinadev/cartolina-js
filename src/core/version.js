4c8eb74b65b49e6587d8437986818101549bae48
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-10-08 19:55:49 +0200

    separate webpack build for loaders, blob-based worker initialization

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.49.114-ondra_cartolina23.0c23cb7';
+  return '2.49.115-ondra_cartolina23.b1f9e3e';
 }
