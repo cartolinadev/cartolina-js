9d35ffb093e5eca332ac82fd55c02f4aa34e345a
Ondra Prochazka <ondrej.prochazka@montevallo.cz>
2025-10-09 22:21:38 +0200

    minor fixes

--- src/core/version.js
+++ src/core/version.js
@@ -2,5 +2,5 @@
 // Use scripts/bump-patch.js to update version.
 
 export default function getVersion() {
-  return '2.49.115-ondra_cartolina23.b1f9e3e';
+  return '2.49.116-ondra_cartolina23.a460d85';
 }
